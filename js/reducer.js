758547ec4266a7b64b9f3b46e09f06e1ebe1bcd3
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-06-28 15:11:27 +1000

    Created a dispatch action to increase counter

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,22 @@
 // add code snippets from README
+let state = {count: 0}
+
+function changeState(state, action) {
+    switch (action.type) {
+        case 'INCREASE_COUNT':
+            return {count: state.count + 1};
+        default:
+            return state;
+    }
+}
+
+function dispatch(action) {
+    state = changeState(state, action);
+    render()
+}
+
+function render() {
+    document.body.textContent = state.count
+}
+
+render()
